f10219779d6ec9d3b608c6c4e4f0afc7fceff14c
root <root@instance-1.asia-east2-a.c.mlhnu-225502.internal>
2019-01-16 16:21:31 +0000

    fix the subrespo

--- blog/blog.sh
+++ blog/blog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /qqx/blog/dev/ && nohup /usr/bin/hexo s -w -p 1234 &
-cd /qqx/blog/sec/ && nohup /usr/bin/hexo s -w -p 1235 &
-cd /qqx/blog/ser/ && nohup /usr/bin/hexo s -w -p 1236 &
+cd /blog/blog/dev/ && nohup /usr/bin/hexo s -w -p 1234 &
+cd /blog/blog/sec/ && nohup /usr/bin/hexo s -w -p 1235 &
+cd /blog/blog/ser/ && nohup /usr/bin/hexo s -w -p 1236 &
 
