0e6e481910d7d3e717920ee4df46fa98aa95cea5
root <root@instance-1.asia-east2-a.c.mlhnu-225502.internal>
2019-01-17 03:05:29 +0000

    none

--- blog/blog.sh
+++ blog/blog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /blog/blog/dev/ && nohup /usr/bin/hexo s -w -p 1234 &
-cd /blog/blog/sec/ && nohup /usr/bin/hexo s -w -p 1235 &
-cd /blog/blog/ser/ && nohup /usr/bin/hexo s -w -p 1236 &
+cd /blog/blog/dev/ && nohup /usr/bin/hexo s -w &
+cd /blog/blog/sec/ && nohup /usr/bin/hexo g -w &
+cd /blog/blog/ser/ && nohup /usr/bin/hexo g -w &
 
