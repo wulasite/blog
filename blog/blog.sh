c0ed23c7d616ecc08eeb40e36db49f9b2b66f6f4
root <root@localhost.localdomain>
2019-04-24 20:13:39 +0800

    update the blog config

--- blog/blog.sh
+++ blog/blog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /blog/blog/dev/ && nohup /usr/bin/hexo s -w &
-cd /blog/blog/sec/ && nohup /usr/bin/hexo g -w &
-cd /blog/blog/ser/ && nohup /usr/bin/hexo g -w &
+cd /blog/blog/dev/ && nohup /usr/bin/hexo g -w -d&
+cd /blog/blog/sec/ && nohup /usr/bin/hexo g -w -d&
+cd /blog/blog/ser/ && nohup /usr/bin/hexo g -w -d&
 
